c4a87c6614bccf2bbb8f2a6e8d4e2f3eb6981aa5
Movieguy3333 <Movieguy3333@gmail.com>
2024-08-11 07:09:11 -0400

    clean up and renaming files

--- public/js/profile.js
+++ public/js/profile.js
@@ -33,7 +33,34 @@ const { response } = require("express");
           alert('Error loading profile');
           window.location.href = 'login.html';
         }
+
+        fetchEvents();
     });
+
+    function fetchEvents() {
+      fetch('/api/events')
+          .then(response => response.json())
+          .then(events => {
+              const eventList = document.getElementById('event-list');
+              eventList.innerHTML = '';
+              events.forEach(element => {
+                  eventList.innerHTML += `
+                      <div class="event-item">
+                      
+                          <h3>Event: ${element.name}</h3>
+                          <p>ID: ${element._id}</p>
+                          <p>Date: ${new Date(element.date).toLocaleDateString()}</p>
+                          <p>Location: ${element.location}</p>
+                          <p>Description: ${element.description}</p>
+                          <p>Availability: ${element.availability}</p>
+                         
+                      </div>
+                  `;
+              });
+          });
+  }
+
+    
     
     function selectProfilePic(src){
         document.querySelectorAll('.picture-gallery img').forEach(img =>{
